a646d444fff8661323ebe51f90f87048cb0b8d43
mevlüt çifcibaşı <nebioo@users.noreply.github.com>
2021-01-10 16:05:25 +0300

    quartz added

---
+++ api/Api/Models/JobSchedule.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Api.Models
+{
+    public class JobSchedule
+    {
+        public JobSchedule(Type jobType, string cronExpression)
+        {
+            JobType = jobType;
+            CronExpression = cronExpression;
+        }
+        public Type JobType { get; }
+        public string CronExpression { get; }
+    }
+}
